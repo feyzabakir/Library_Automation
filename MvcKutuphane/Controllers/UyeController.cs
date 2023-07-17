517dd5ac75648872d4f37691f10e411b128baaca
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-17 20:45:43 +0300

    Add project files.

---
+++ MvcKutuphane/Controllers/UyeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class UyeController : Controller
+    {
+        // GET: Uye
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLUYELER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UyeEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UyeEkle(TBLUYELER p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UyeEkle");
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
