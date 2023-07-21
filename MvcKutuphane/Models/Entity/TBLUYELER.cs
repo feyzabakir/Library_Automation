49ccfba0dc90dbc3c87eb9bc423a635e481eebcb
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-21 15:07:29 +0300

    loaned

--- MvcKutuphane/Models/Entity/TBLUYELER.cs
+++ MvcKutuphane/Models/Entity/TBLUYELER.cs
@@ -11,8 +11,7 @@ namespace MvcKutuphane.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class TBLUYELER
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -23,13 +22,10 @@ namespace MvcKutuphane.Models.Entity
         }
     
         public int ID { get; set; }
-        [Required(ErrorMessage ="Adý boþ býrakamazsýnýz")]
-        [StringLength(20, ErrorMessage ="En fazla 20 karakter girebilirsiniz")]
         public string AD { get; set; }
         public string SOYAD { get; set; }
         public string MAIL { get; set; }
         public string KULLANICIADI { get; set; }
-        [StringLength(10, ErrorMessage = "En fazla 10 karakter girebilirsiniz")]
         public string SIFRE { get; set; }
         public string FOTOGRAF { get; set; }
         public string TELEFON { get; set; }
