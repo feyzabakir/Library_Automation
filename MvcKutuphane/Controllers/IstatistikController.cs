fb74d2419b63ba04ebe910aaa2d6db1b9741027f
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-27 22:48:58 +0300

    Dashboard

---
+++ MvcKutuphane/Controllers/IstatistikController.cs
@@ -0,0 +1,27 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        // GET: Istatistik
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var deger1 = db.TBLUYELER.Count();
+            var deger2 = db.TBLKITAP.Count();
+            var deger3 = db.TBLKITAP.Where(x => x.DURUM == false).Count();
+            var deger4 = db.TBLCEZALAR.Sum(x => x.PARA);
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            ViewBag.dgr4 = deger4;
+            return View();
+        }
+    }
+}
