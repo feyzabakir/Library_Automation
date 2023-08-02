3b6549c422076ef40a3d2209a1524990906f8ad2
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-08-02 23:03:47 +0300

    Login

---
+++ MvcKutuphane/Controllers/MesajlarController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class MesajlarController : Controller
+    {
+        // GET: Mesajlar
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult YeniMesaj()
+        {
+            return View();
+        }
+    }
+}
--- MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/Controllers/PanelimController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,27 @@ namespace MvcKutuphane.Controllers
     public class PanelimController : Controller
     {
         // GET: Panelim
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var uyemail = (string)Session["Mail"];
+            var degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            return View(degerler);
+
+        }
+        [HttpPost]
+        public ActionResult Index2(TBLUYELER p)
+        {
+            var kullanici = (string)Session["Mail"];
+            var uye = db.TBLUYELER.FirstOrDefault(x => x.MAIL == kullanici);
+            uye.SIFRE = p.SIFRE;
+            uye.AD = p.AD;
+            uye.FOTOGRAF = p.FOTOGRAF;
+            uye.OKUL = p.OKUL;
+            uye.KULLANICIADI = p.KULLANICIADI;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
